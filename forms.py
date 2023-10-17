88e59c76d585b32bb78d1e83c4f1877dd1470037
kathichro <s_chroszczinsky21@stud.hwr-berlin.de>
2023-10-17 21:15:27 +0200

    Eweiterung von forms.py um User-Login

--- forms.py
+++ forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms.fields import BooleanField, HiddenField, SelectField, StringField, SubmitField
-from wtforms.validators import InputRequired, Length
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import  InputRequired, Length, ValidationError
+#from db import db
 
 class CreateTodoForm(FlaskForm):
     description = StringField(validators=[InputRequired(), Length(min=5)])
@@ -13,3 +15,32 @@ class TodoForm(FlaskForm):
     description = StringField(validators=[InputRequired()])
     list_id = SelectField(coerce=int, choices=[], validate_choice=False)
     submit = SubmitField('Update')
+
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(
+        min=5, max=30)], render_kw={"placeholder": "Benutzername"})
+
+    password = PasswordField(validators=[InputRequired(), Length(
+        min=5, max=25)], render_kw={"placeholder": "Passwort"})
+
+    submit = SubmitField("Register") #muss ich hier register nhemen wegen des Klassennamens oder ist das egal?
+
+    
+  # def validate_username(self, username):
+  #     existing_user_username = db.User.query.filter_by(
+  #         username=username.data).first()
+  #     
+  #     if existing_user_username:
+  #         raise ValidationError(
+  #             "Dieser Benutzername existiert bereits. Bitte nehmen Sie einen anderen Namen!"
+  #         )
+        
+
+class LoginForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(
+        min=5, max=30)], render_kw={"placeholder": "Benutzername"})
+
+    password = PasswordField(validators=[InputRequired(), Length(
+        min=5, max=25)], render_kw={"placeholder": "Passwort"})
+
+    submit = SubmitField("Login")
