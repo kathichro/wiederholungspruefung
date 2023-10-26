1d2cc2975b265b21b9529083df33f85d73a36645
kathichro <s_chroszczinsky21@stud.hwr-berlin.de>
2023-10-27 01:38:40 +0200

    Kommentare hinzugefügt + Namen geändert forms.py

--- forms.py
+++ forms.py
@@ -2,7 +2,6 @@ from flask_wtf import FlaskForm
 from wtforms.fields import BooleanField, HiddenField, SelectField, StringField, SubmitField
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import  InputRequired, Length, ValidationError
-#from db import db
 
 class CreateTodoForm(FlaskForm):
     description = StringField(validators=[InputRequired(), Length(min=5)])
@@ -16,24 +15,26 @@ class TodoForm(FlaskForm):
     list_id = SelectField(coerce=int, choices=[], validate_choice=False)
     submit = SubmitField('Update')
 
+#Registrierungs-Form hinzugefügt
 class RegisterForm(FlaskForm):
     username = StringField(validators=[InputRequired(), Length(
-        min=5, max=30)], render_kw={"placeholder": "Benutzername"})
+        min=5, max=30)], render_kw={"placeholder": "username"})
 
     password = PasswordField(validators=[InputRequired(), Length(
-        min=5, max=25)], render_kw={"placeholder": "Passwort"})
+        min=5, max=25)], render_kw={"placeholder": "password"})
 
-    submit = SubmitField("Register") #muss ich hier register nhemen wegen des Klassennamens oder ist das egal?
+    submit = SubmitField("REGISTER")
         
+#Login-Form hinzugefügt        
 class LoginForm(FlaskForm):
     username = StringField(validators=[InputRequired(), Length(
-        min=5, max=30)], render_kw={"placeholder": "Benutzername"})
+        min=5, max=30)], render_kw={"placeholder": "username"})
 
     password = PasswordField(validators=[InputRequired(), Length(
-        min=5, max=25)], render_kw={"placeholder": "Passwort"})
+        min=5, max=25)], render_kw={"placeholder": "password"})
 
-    submit = SubmitField("Login")
+    submit = SubmitField("LOGIN")
 
-
+#Account löschen-Form hinzugefügt
 class DeleteAccount(FlaskForm):
     submit = SubmitField('DELETE ACCOUNT')
